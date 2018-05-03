f74be4279061fea892c83f322f671171c50a56d6
Vykhor Marta <vykhor.m@gmail.com>
2018-05-03 22:35:01 +0300

    add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,20 @@ import { QuestionService } from './services/question.service';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MainImageComponent } from './header/main-image/main-image.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ListQuestionsComponent,
-    QuestionComponent
+    QuestionComponent,
+    HeaderComponent,
+    MainImageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
